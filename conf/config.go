3dc24ae3db2a51251ffcf31cd462b6431bb83b15
yanjiankai <yanjiankai@msh.team>
2024-02-18 18:36:42 +0800

    viper兼容配置文件和结构体模式

--- conf/config.go
+++ conf/config.go
@@ -1,46 +1,51 @@
-package config
+package conf
 
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"strings"
 )
 
 // ConfigManager 是一个管理配置的接口
 type ConfigManager interface {
-	Init(viperConfig interface{}) *viper.Viper
+	InitConfig(viperConfig interface{}) *viper.Viper
 }
 
 // ViperConfigManager 是 ConfigManager 接口的具体实现
 type ViperConfigManager struct {
-	configPath string
-	configName string
-	configType string
+	ConfigPath string
+	ConfigName string
+	ConfigType string
 }
 
-// Init 初始化 Viper，如果提供了结构体，将配置映射到该结构体；如果没有提供结构体，直接返回 Viper 对象
-func (vc *ViperConfigManager) Init(viperConfig interface{}) (*viper.Viper, error) {
+// InitConfig 初始化 Viper，如果提供了结构体，将配置映射到该结构体；如果没有提供结构体，直接返回 Viper 对象
+func (vc *ViperConfigManager) InitConfig(viperConfig interface{}) (*viper.Viper, error) {
 	// 初始化 Viper
-	viper.AddConfigPath(vc.configPath)
-	viper.SetConfigName(vc.configType)
-	viper.SetConfigType(vc.configType)
+	viper.AddConfigPath(vc.ConfigPath)
+	viper.SetConfigName(vc.ConfigName)
+	//viper.SetConfigType(vc.ConfigType)
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
-
 	// 读取配置文件
 	if err := viper.ReadInConfig(); err != nil {
 		// 如果读取配置文件失败，记录错误并返回 Viper 对象
-		return nil, fmt.Errorf("Error reading config file: %w", err)
+		return nil, fmt.Errorf("Error reading conf file: %w", err)
 		//return viper.GetViper()
 	}
 
+	// 设置环境变量前缀
+	//viper.SetEnvPrefix("DEMO")
+	//viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	// 如果提供了结构体，尝试将其映射到配置
+
 	if viperConfig != nil {
 		if err := viper.Unmarshal(viperConfig); err != nil {
 			// 如果映射配置到结构体失败，记录错误并返回 Viper 对象
-			return nil, fmt.Errorf("Error unmarshaling config to struct: %w", err)
+			return nil, fmt.Errorf("Error unmarshaling conf to struct: %w", err)
 
 		}
 	}
+	fmt.Println(viper.AllSettings())
 
-	// 返回 Viper 对象
 	return viper.GetViper(), nil
 }
--- example/config/config_test.go
+++ example/config/config_test.go
@@ -1,21 +1,53 @@
-package config
+package conf
 
 import (
-	_ "github.com/tsopia/gokit/conf"
+	"github.com/tsopia/gokit/conf"
+	"os"
+
 	"testing"
 )
 
-func TestViperConfigManager(t *testing.T) {
-	//config.ViperConfigManager{}
-	//vcm := ViperConfigManager("./", "config", "json")
-	//
-	//v := vcm.Init(nil)
-	//if v == nil {
-	//	t.Errorf("Expected viper instance, got nil")
-	//}
-	//
-	//err := v.ReadInConfig()
+// func TestViperConfigManager(t *testing.T) {
+//
+// }
+// 编写InitConfig方法的测试用例
+func TestInitConfig(t *testing.T) {
+	// 初始化 ViperConfigManager
+	os.Setenv("APP_NAME", "DEMO")
+	os.Setenv("host", "DDD")
+	//os.Setenv("host", "CCC")
+	os.Setenv("PASS", "pass")
+	director, _ := os.Getwd()
+	vcm := &conf.ViperConfigManager{
+		ConfigPath: director,
+		ConfigType: "yaml",
+		ConfigName: "config",
+	}
+
+	// 初始化配置, 读取配置文件
+	//c, err := vcm.InitConfig(nil)
 	//if err != nil {
-	//	t.Errorf("Expected no error, got %v", err)
+	//	t.Errorf("InitConfig() failed, err: %v", err)
 	//}
+	//port := c.GetString("port")
+	//t.Logf("port: %v", port)
+	// 初始化配置, 读取配置文件, 并将配置映射到结构体
+	configStruct := &Config{}
+	cc, err := vcm.InitConfig(configStruct)
+	if err != nil {
+		t.Errorf("InitConfig() failed, err: %v", err)
+	}
+	t.Logf("configStruct.port: %v", configStruct.Port)
+	t.Logf("configStruct.Host: %v", configStruct.Host)
+	t.Logf("configStruct.name: %v", configStruct.Name)
+	t.Logf("configStruct.path: %v", configStruct.Path)
+	t.Logf("configStruct.DEMO_DB_HOST: %v", cc.GetString("HOST"))
+
+}
+
+type Config struct {
+	Port string `mapstructure:"port"`
+	Host string `mapstructure:"host"`
+	Name string `mapstructure:"name"`
+	Path string `mapstructure:"path"`
 }
